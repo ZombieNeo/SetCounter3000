b1f1a4d1cf565d836d54a4965285da7e4b907286
ZombieNeo <46230458+ZombieNeo@users.noreply.github.com>
2020-10-08 10:43:59 +0100

    REWORK made better! with welcome message

--- saveload.py
+++ saveload.py
@@ -3,7 +3,7 @@ def save_system():
     doload = "y"
     if doload == "y" or "Y":
         global date
-        date= input("enter save name (date) ")
+        date= input("enter save name (DEBUG: THIS IS LOADING SYSTEM!) ")
         s = open("%s.txt" % date, "a")
         print("LOADED SUCCESSFULLY")
         pass
